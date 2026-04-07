e28c46dba6f7f496f5aad1f9743a189800d4246e
Aliphtechnology <aliphtechie@gmail.com>
2026-04-08 00:48:41 +0800

    Create index.js to render the Home component

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// pages/index.js
+// Import the existing page component you already have at repo root.
+// This preserves your layout and design exactly as in page.js.
+import Page from '../page';
+
+export default function Home() {
+  return <Page />;
+}
