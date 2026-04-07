11b4461408fde56754dc450e5f53596908d65e46
mycosg <alphaoak1@gmail.com>
2026-04-08 02:19:06 +0800

    pages delete

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-// pages/_app.js
-import '../globals.css'; // load your existing global styles
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-// pages/index.js
-// Import the existing page component you already have at repo root.
-// This preserves your layout and design exactly as in page.js.
-import Page from '../page';
-
-export default function Home() {
-  return <Page />;
-}
